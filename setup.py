e823329c1f64029fddef02bbb3771995863b62e9
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-12 18:14:53 +0900

    Remove a temporary variable

--- setup.py
+++ setup.py
@@ -39,8 +39,6 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 SETUPTOOLS_REQUIRES = ["setuptools>=38.3.0"]
 PYTEST_RUNNER_REQUIRES = ["pytest-runner"] if need_pytest() else []
-cmdclass = {}
-cmdclass.update(get_release_command_class())
 
 setuptools.setup(
     name=MODULE_NAME,
@@ -79,5 +77,5 @@ setuptools.setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Testing",
     ],
-    cmdclass=cmdclass,
+    cmdclass=get_release_command_class(),
 )
