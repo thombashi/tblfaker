847e9125ec001545b913d7efd86fa20662333ad6
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-02-09 13:37:41 +0900

    Update __copyright__

--- tblfaker/__version__.py
+++ tblfaker/__version__.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-
-
 __author__ = "Tsuyoshi Hombashi"
-__copyright__ = "Copyright {}, {}".format(datetime.now().year, __author__)
+__copyright__ = "Copyright 2018, {}".format(__author__)
 __license__ = "MIT License"
 __version__ = "0.1.0"
 __maintainer__ = __author__
