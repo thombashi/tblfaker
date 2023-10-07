c43973d23a76f4b655531f9dc1c850dee6ef58f2
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-10-07 17:59:34 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setuptools.setup(
         "test": all_requires + TESTS_REQUIRES,
     },
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: MIT License",
--- tblfaker/__version__.py
+++ tblfaker/__version__.py
@@ -4,6 +4,6 @@ from typing import Final
 __author__: Final = "Tsuyoshi Hombashi"
 __copyright__: Final = f"Copyright 2018, {__author__}"
 __license__: Final = "MIT License"
-__version__: Final = "0.2.4"
+__version__: Final = "1.0.0"
 __maintainer__: Final = __author__
 __email__: Final = "tsuyoshi.hombashi@gmail.com"
